19a32df534abe02858652cfb4e54af26eae15d41
Alex Leventer <alexleventer@gmail.com>
2019-01-03 22:08:09 -0800

    add the rest of endpoints

---
+++ src/resources/CallDispositions.ts
@@ -0,0 +1,11 @@
+import Resource from '../Resource';
+
+export default class CallDispositions extends Resource {
+  private readonly endpoint:string;
+  private readonly apiKey:string;
+  constructor(endpoint:string, apiKey:string) {
+    super();
+    this.endpoint = endpoint;
+    this.apiKey = apiKey;
+  }
+}
