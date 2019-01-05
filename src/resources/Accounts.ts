1f2eca4777ae09ddfed3babc6b11e247f1173f1c
Alex Leventer <alexleventer@gmail.com>
2019-01-04 19:13:37 -0800

    add support for update account

--- src/resources/Accounts.ts
+++ src/resources/Accounts.ts
@@ -20,6 +20,10 @@ export default class Accounts extends Resource {
   async create(data: AccountRequest) {
     return await super.create(data);
   }
+
+  async update(id: string, data: AccountRequest) {
+    return await super.update(id, data);
+  }
 }
 
 export interface AccountRequest {
@@ -31,4 +35,24 @@ export interface AccountRequest {
   website?: string;
   linkedin_url?: string;
   twitter_handle?: string;
+  street?: string;
+  city?: string;
+  state?: string;
+  postal_code?: string;
+  country?: string;
+  locale?: string;
+  industry?: string;
+  company_type?: string;
+  founded?: string;
+  revenue_range?: string;
+  size?: string;
+  do_not_contact?: boolean;
+  custom_fields?: string[];
+  tags?: string[];
+  owner_id?: number;
+  company_stage_id?: number;
+}
+
+export interface AccountResult {
+
 }
