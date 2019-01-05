a0cbe8786d9d92d7f80e2311bb30671082c785ab
Alex Leventer <alexleventer@gmail.com>
2019-01-04 17:16:15 -0800

    fix return types

--- __tests__/SalesLoft.test.ts
+++ __tests__/SalesLoft.test.ts
@@ -2,6 +2,8 @@ import SalesLoft from '../src/SalesLoft';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import {PersonRequest} from '../src/resources/People';
+import {CurrentUserResponse} from '../src/resources/Me';
+import {TeamResponse} from '../src/resources/Team';
 const { API_KEY }:any = process.env;
 
 describe('SalesLoft', async () => {
@@ -9,7 +11,7 @@ describe('SalesLoft', async () => {
   const { Me, Team, Accounts, Cadences, ActionDetails, Actions, People, CadenceMemberships, Users } = sl;
   describe('Me', async () => {
     it('should fetch current user', async () => {
-      const meResults = await Me.fetchCurrentUser();
+      const meResults:CurrentUserResponse = await Me.fetchCurrentUser();
       expect(meResults).to.have.property('data');
       expect(meResults.data).to.have.property('id');
       expect(meResults.data).to.have.property('guid');
@@ -19,7 +21,7 @@ describe('SalesLoft', async () => {
   });
   describe('Team', async () => {
     it('should fetch current team', async () => {
-      const teamResults = await Team.fetchCurrentTeam();
+      const teamResults:TeamResponse = await Team.fetchCurrentTeam();
       expect(teamResults).to.have.property('data');
       expect(teamResults.data).to.have.property('id');
       expect(teamResults.data).to.have.property('name');
--- src/resources/Team.ts
+++ src/resources/Team.ts
@@ -19,21 +19,23 @@ export default class Team extends Resource {
 }
 
 export interface TeamResponse {
-  id: number;
-  name: string;
-  created_at: string;
-  updated_at: string;
-  group_privacy_setting: string;
-  team_visibility_default: string;
-  plan: string;
-  license_limit: number;
-  local_dial_enabled: boolean;
-  record_by_default: boolean;
-  call_recording_disabled: boolean;
-  email_daily_limit: number;
-  click_tracking_default: boolean;
-  allow_automated_email_steps: boolean;
-  custom_tracking_domain: string;
-  dispositions_required: boolean;
-  sentiments_required: boolean;
+  data: {
+    id: number;
+    name: string;
+    created_at: string;
+    updated_at: string;
+    group_privacy_setting: string;
+    team_visibility_default: string;
+    plan: string;
+    license_limit: number;
+    local_dial_enabled: boolean;
+    record_by_default: boolean;
+    call_recording_disabled: boolean;
+    email_daily_limit: number;
+    click_tracking_default: boolean;
+    allow_automated_email_steps: boolean;
+    custom_tracking_domain: string;
+    dispositions_required: boolean;
+    sentiments_required: boolean;
+  }
 }
